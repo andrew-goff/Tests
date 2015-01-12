b2409867289b41c7fac6b11ba7a6c63a30a8acb8
andrew-goff <andrew.goff@hotmail.co.uk>
2015-01-12 19:31:46 +0000

    FriendlyArmy Test
    Tests the constructor and method in FriendlyArmy class

---
+++ FriendlyArmyTest.cs
@@ -0,0 +1,94 @@
+﻿using ConsoleDrivenBattleGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using EnemyFighters;
+using System.Collections.Generic;
+
+namespace FriendlyArmyTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for FriendlyArmyTest and is intended
+    ///to contain all FriendlyArmyTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class FriendlyArmyTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for create
+        ///</summary>
+        [TestMethod()]
+        public void createTest()
+        {
+            FriendlyArmy target = new FriendlyArmy(); 
+
+            int count = 10;
+            List<IFighter> actual;
+
+            actual = target.create(count);
+
+            Assert.AreEqual(count, actual.Count);
+        }
+
+        /// <summary>
+        ///A test for FriendlyArmy Constructor
+        ///</summary>
+        [TestMethod()]
+        public void FriendlyArmyConstructorTest()
+        {
+            FriendlyArmy target = new FriendlyArmy();
+        }
+    }
+}
